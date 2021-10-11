8b03b239f33cc41444fc798e9723711539dc7c59
Tanmay Hiremath <tanmay.hiremath@gmail.com>
2021-10-12 04:59:42 +0530

    3

--- pdf_handler/settings.py
+++ pdf_handler/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,5 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-import django_heroku
+
 django_heroku.settings(locals())
