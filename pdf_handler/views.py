57d08bcd4e94c84deb50ddff0b50d8df6589b326
Tanmay Hiremath <tanmay.hiremath@gmail.com>
2021-10-12 05:19:05 +0530

    8

--- pdf_handler/views.py
+++ pdf_handler/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from django.http import JsonResponse
+from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 from PIL import Image
+from wsgiref.util import FileWrapper
+import os
 # Create your views here.
 class ImgToPdf(APIView):
       def get(self, request):
@@ -14,4 +16,10 @@ class ImgToPdf(APIView):
             home_img = img_list[0]
             print(home_img)
             home_img.save('xyz.pdf', "PDF" ,resolution=100.0, save_all=True, append_images=img_list[1:])
-            return JsonResponse({'data': 'uploaded'}, status=200)
+            filename = 'xyz.pdf'
+            content = FileWrapper(filename)
+            response = HttpResponse(content, content_type='application/pdf')
+            response['Content-Length'] = os.path.getsize(filename)
+            response['Content-Disposition'] = 'attachment; filename=%s' % 'converted.pdf'
+            return response
+    
